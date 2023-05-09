737142592828eadd3734da62f9db08ba4dc3c383
audemard <gilles.audemard@univ-artois.fr>
2023-05-09 11:11:03 +0200

    Glucose 2.0

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Glucose SAT solver
 
-This is the first release (1.0) of the glucose SAT solver. 
-It is based on [Minisat 1.14](http://minisat.se/MiniSat.html)
+This is the release 2.0 of the glucose SAT solver. 
+It is based on [Minisat 2.2](http://minisat.se/MiniSat.html)
 
 For compiling:  ```./build.sh```
 
--- sources/glucose/core/BoundedQueue.h
+++ sources/glucose/core/BoundedQueue.h
@@ -1,16 +1,17 @@
-/*****************************************************************************************[Queue.h]
-MiniSat -- Copyright (c) 2003-2006, Niklas Een, Niklas Sorensson
-  2008 - Gilles Audemard, Laurent Simon
-
+/***********************************************************************************[BoundedQueue.h]
+ Glucose -- Copyright (c) 2009, Gilles Audemard, Laurent Simon
+                CRIL - Univ. Artois, France
+                LRI  - Univ. Paris Sud, France
+ 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
 including without limitation the rights to use, copy, modify, merge, publish, distribute,
 sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
+ 
 The above copyright notice and this permission notice shall be included in all copies or
 substantial portions of the Software.
-
+ 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
 NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
@@ -18,13 +19,15 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **************************************************************************************************/
 
+
 #ifndef BoundedQueue_h
 #define BoundedQueue_h
 
-#include "Vec.h"
+#include "mtl/Vec.h"
 
 //=================================================================================================
 
+namespace Minisat {
 
 template <class T>
 class bqueue {
@@ -73,6 +76,7 @@ public:
 
 
 };
-
+}
 //=================================================================================================
+
 #endif
